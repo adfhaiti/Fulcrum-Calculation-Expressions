146a29510cb9cc5b33ce91313c9321b717eb6738
adfhaiti <josiah@adfhaiti.org>
2022-09-03 00:04:20 -0400

    Just storing this in case I ever clean it up and use it for something.

---
+++ Temp - To Sort - Clean Eventually.js
@@ -0,0 +1,19 @@
+if (CHOICEVALUE($chc_1_nan_ki_depatman_kay_la_ye) == "Lwès, oswa Grandans") {
+  var sko_depatman = 0;
+  SETRESULT(sko_depatman);
+}
+
+if (CHOICEVALUE($chc_1_nan_ki_depatman_kay_la_ye) == "Nòdwès, oswa Sid") {
+  var sko_depatman = 4;
+  SETRESULT(sko_depatman);
+}
+
+if (CHOICEVALUE($chc_1_nan_ki_depatman_kay_la_ye) == "Latibonit, oswa Nip") {
+  var sko_depatman = 9;
+  SETRESULT(sko_depatman);
+}
+
+if (CHOICEVALUE($chc_1_nan_ki_depatman_kay_la_ye) == "Nò, oswa Sidès") {
+  var sko_depatman = 9;
+  SETRESULT(sko_depatman);
+}
